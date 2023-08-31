fffdb2a88225e77c9aff5117e0d62eadfdc2b3e2
Nathaniel D'Rozario <nsdrozario@gmail.com>
2023-08-31 12:19:41 -0700

    added license and better filter distribution

--- src/VocoderFilterDistributor.cpp
+++ src/VocoderFilterDistributor.cpp
@@ -1,3 +1,21 @@
+/*
+    This file is a part of libvocoder, a library for audio effects
+    done using bandpass filter banks.
+    Copyright (C) 2023 Nathaniel D'Rozario
+
+    libvocoder is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 #include <VocoderFilterDistributor.hpp>
 #include <cmath>
 using namespace Vocoder;
@@ -14,9 +32,13 @@ float VocoderFilterDistributor::getCenterFreq(int i) const {
     // start * (base ^ n) = end
     // end/start = base ^ n
     // base = nth root of (end/start)
-    return start * pow(end/start, 1.0f/i); 
+    // if not i+1 then we get a division by zero error
+    float base = pow(end/start, 1.0f/n);
+    return start * (pow(base, i)); 
 }
 
 float VocoderFilterDistributor::getQ(int i) const {
-    return 4;
+    // https://en.wikipedia.org/wiki/Constant-Q_transform;
+    float bandwith = pow(pow(2, 1.0f/n), i) * start;
+    return getCenterFreq(i)/bandwith;
 }
--- src/VocoderTest.cpp
+++ src/VocoderTest.cpp
@@ -1,16 +1,38 @@
+/*
+    This file is a part of libvocoder, a library for audio effects
+    done using bandpass filter banks.
+    Copyright (C) 2023 Nathaniel D'Rozario
+
+    libvocoder is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 #include <vocoder/VocoderProcessor.hpp>
 #include <vocoder/AudioFileReader.hpp>
 #include <vocoder/AudioFileWriter.hpp>
 #include <cmath>
+#include <ctime>
+
 using namespace Vocoder;
 
 int main(int argc, char *argv[]) {
 
     if (argc < 2) { exit(0); }
 
+    srand(time(nullptr));
+
     AudioFileReader a (argv[1]);
     ma_decoder& decoder = a.getDecoderInstance();
-    AudioFileWriter w ("out.mp3", decoder.outputSampleRate, ma_encoding_format_wav);
+    AudioFileWriter w ("out.wav", decoder.outputSampleRate, ma_encoding_format_wav);
     VocoderProcessor proc (decoder.outputSampleRate, 30, 50, 8000);
 
     float sample = 0.0f;
